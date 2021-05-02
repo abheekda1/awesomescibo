ad748abf27a5729154aa4c76af4d8ac326faa767
Abheek Dhawan <abheekd@protonmail.com>
2021-05-02 12:09:00 -0500

    Remove fs and execSync as they aren't used anymore

--- bot/awesomescibo.js
+++ bot/awesomescibo.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const Discord = require("discord.js");
-const execSync = require("child_process").execSync;
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
