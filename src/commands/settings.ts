559b4e7e7cdc75a7829c7bab4328906ed3536cb0
Eric Yang <64386991+xlq902@users.noreply.github.com>
2022-04-14 18:59:07 -0400

    Update settings.ts
    fixes majority of linting issues

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,25 +1,23 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+import { MessageActionRow, MessageSelectMenu } from 'discord.js';
 import { CommandInteraction, MessageEmbed } from 'discord.js';
 
-import log from '../helpers/log';
-
 export const data = new SlashCommandBuilder()
-    .setName('settings')
-    .setDescription('BETA - settings configuration')
-    .addSubcommand(subcommand => {
+	.setName('settings')
+	.setDescription('BETA - settings configuration')
+	.addSubcommand(subcommand => {
 		subcommand
 			.setName('subject')
 			.setDescription('Changes subject of problems');
 		return subcommand;
 	})
-    .addSubcommand(subcommand => {
+	.addSubcommand(subcommand => {
 		subcommand
 			.setName('display')
 			.setDescription('Displays current settings');
 		return subcommand;
 	})
-    .addSubcommand(subcommand => {
+	.addSubcommand(subcommand => {
 		subcommand
 			.setName('gradelevels')
 			.setDescription('Changes grade level of problems');
@@ -28,213 +26,204 @@ export const data = new SlashCommandBuilder()
 ;
 
 export async function execute(interaction : CommandInteraction) {
-    const action = interaction.options.getSubcommand();
+	const action = interaction.options.getSubcommand();
 	switch (action) {
-        case 'display': {
-            await interaction.deferReply(); 
-            const settingsEmbed = new MessageEmbed()
-                .setColor('#ffffff');
+	case 'display': {
+		await interaction.deferReply();
+		const settingsEmbed = new MessageEmbed()
+			.setColor('#ffffff');
 
-            const user = interaction.options.getUser('user') || interaction.user;
-    
-            const client = interaction.client;
+		const user = interaction.options.getUser('user') || interaction.user;
 
-            settingsEmbed
-              .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
-                .setDescription(`Current selections: `);
-              const menu = new MessageActionRow() 
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId('select')
-                        .setPlaceholder('Nothing selected')
-                       .addOptions([
-                        {
-                            label: 'subjects',
-                            description: 'subjects',
-                            value: 'subjects',
-                        },
-                        {
-                            label: 'gradelevels',
-                            description: 'grade levels',
-                            value: 'gradelevels',
-                         },
-                        ]),
-                );
+		const client = interaction.client;
 
-            await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
-    
-            client.on('interactionCreate', async interaction => {
-            if (!interaction.isSelectMenu()) return;
-            var values = interaction.values[0]; 
-            switch(values)  {
-                case 'subjects':
-                    await interaction.update({ content: 'subjects was selected!', components: [] });
-                    break;
-                case 'gradelevels':
-                    await interaction.update({ content: 'levels was selected!', components: [] });
-                    break;
-                }
-            });
-            break;
-        }
-        case 'gradelevels': {
-            await interaction.deferReply(); 
-            const settingsEmbed = new MessageEmbed()
-                .setColor('#ffffff');
+		settingsEmbed
+			.setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+			.setDescription('Current selections: ');
+		const menu = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'subjects',
+							description: 'subjects',
+							value: 'subjects',
+						},
+						{
+							label: 'gradelevels',
+							description: 'grade levels',
+							value: 'gradelevels',
+						},
+					]),
+			);
 
-            const user = interaction.options.getUser('user') || interaction.user;
-    
-            const client = interaction.client;
+		await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
 
-            settingsEmbed
-              .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
-                .setDescription(`Current level settings: `);
-              const menu = new MessageActionRow()
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId('select')
-                        .setPlaceholder('Nothing selected')
-                       .addOptions([
-                        {
-                            label: 'All',
-                            description: 'All',
-                            value: 'all',
-                        },
-                        {
-                            label: 'Middle School',
-                            description: 'Middle school',
-                            value: 'ms',
-                        },
-                        {
-                            label: 'High School',
-                            description: 'High school',
-                            value: 'hs',
-                         },
-                        ]),
-                );
+		client.on('interactionCreate', async interaction => {
+			if (!interaction.isSelectMenu()) return;
+			const values = interaction.values[0];
+			switch (values) {
+			case 'subjects':
+				await interaction.update({ content: 'subjects was selected!', components: [] });
+				break;
+			case 'gradelevels':
+				await interaction.update({ content: 'levels was selected!', components: [] });
+				break;
+			}
+		});
+		break;
+	}
+	case 'gradelevels': {
+		await interaction.deferReply();
+		const settingsEmbed = new MessageEmbed()
+			.setColor('#ffffff');
 
-            await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
-    
-            client.on('interactionCreate', async interaction => {
-            if (!interaction.isSelectMenu()) return;
-            var values = interaction.values[0]; 
-            switch(values)  {
-                case 'all':
-                    await interaction.update({ content: 'Level set to: All', components: [] });
-                    break;
-                case 'ms':
-                    await interaction.update({ content: 'Level set to: Middle School', components: [] });
-                    break;
-                case 'hs':
-                    await interaction.update({ content: 'Level set to: High School', components: [] });
-                    break;
-                }
-            });
-            break;
-        }
-        case 'subject': {
-            await interaction.deferReply(); 
-           
-            const settingsEmbed = new MessageEmbed()
-                .setColor('#ffffff');
+		const user = interaction.options.getUser('user') || interaction.user;
 
-            const user = interaction.options.getUser('user') || interaction.user;
+		const client = interaction.client;
 
-            const client = interaction.client;
+		settingsEmbed
+			.setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+			.setDescription('Current level settings: ');
+		const menu = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'All',
+							description: 'All',
+							value: 'all',
+						},
+						{
+							label: 'Middle School',
+							description: 'Middle school',
+							value: 'ms',
+						},
+						{
+							label: 'High School',
+							description: 'High school',
+							value: 'hs',
+						},
+					]),
+			);
 
-            settingsEmbed
-                .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
-                .setDescription(`Current subject settings: `);
-            const menu = new MessageActionRow()
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId('select')
-                        .setPlaceholder('Nothing selected')
-                        .addOptions([
-                        {
-                            label: 'All',
-                            description: 'All subjects: astronomy, biology, earth science, chemistry, physics, mathematics, and energy',
-                            value: 'potpourri',
-                        },
-                        {
-                            label: 'Astronomy',
-                            description: 'Astronomy',
-                            value: 'astro',
-                        },
-                        {
-                            label: 'Biology',
-                            description: 'Biology',
-                            value: 'bio',
-                        },
-                        {
-                            label: 'Earth Science',
-                            description: 'Earth Science',
-                            value: 'es',
-                        },
-                        {
-                            label: 'Chemistry',
-                            description: 'Chemistry',
-                            value: 'chem',
-                        },
-                        {
-                            label: 'Physics',
-                            description: 'Physics',
-                            value: 'phy',
-                        },
-                        {
-                            label: 'Mathematics',
-                            description: 'Mathematics',
-                            value: 'math',
-                        },
-                        {
-                            label: 'Energy',
-                            description: 'Energy',
-                            value: 'energy',
-                        }
-                        ]),
-                );
+		await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
 
-            await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
+		client.on('interactionCreate', async interaction => {
+			if (!interaction.isSelectMenu()) return;
+			const values = interaction.values[0];
+			switch (values) {
+			case 'all':
+				await interaction.update({ content: 'Level set to: All', components: [] });
+				break;
+			case 'ms':
+				await interaction.update({ content: 'Level set to: Middle School', components: [] });
+				break;
+			case 'hs':
+				await interaction.update({ content: 'Level set to: High School', components: [] });
+				break;
+			}
+		});
+		break;
+	}
+	case 'subject': {
+		await interaction.deferReply();
 
-            client.on('interactionCreate', async interaction => {
-            if (!interaction.isSelectMenu()) return;
-            var values = interaction.values[0]; 
-            switch(values)  {
-                case 'potpourri':
-                    //everything stuff
-                    await interaction.update({ content: 'Subject set to: Everything', components: [] });
-                    break;
-                case 'astro':
-                    //astronomy stuff
-                    await interaction.update({ content: 'Subject set to: Astronomy', components: [] });
-                    break;
-                case 'bio':
-                    //biology stuff
-                    await interaction.update({ content: 'Subject set to: Biology', components: [] });
-                    break;
-                case 'es':
-                    //earth science stuff
-                    await interaction.update({ content: 'Subject set to: Earth Science', components: [] });
-                    break;
-                case 'chem':
-                    //chemistry stuff
-                    await interaction.update({ content: 'Subject set to: Chemistry', components: [] });
-                    break;
-                case 'phy':
-                    //physics stuff
-                    await interaction.update({ content: 'Subject set to: Physics', components: [] });
-                    break;
-                case 'math':
-                    //math stuff
-                    await interaction.update({ content: 'Subject set to: Mathematics', components: [] });
-                    break;
-                case 'energy':
-                    //the united states department of energy is a cabinet-level agency responsible for the creation and management of...
-                    await interaction.update({ content: 'Subject set to: Energy', components: [] });
-                    break;
-                }
-            });
-            break;
-        }
-    }
+		const settingsEmbed = new MessageEmbed()
+			.setColor('#ffffff');
+
+		const user = interaction.options.getUser('user') || interaction.user;
+
+		const client = interaction.client;
+
+		settingsEmbed
+			.setAuthor({ name: user.tag, iconURL: user.displayAvatarURL() })
+			.setDescription('Current subject settings: ');
+		const menu = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'Astronomy',
+							description: 'Astronomy',
+							value: 'astro',
+						},
+						{
+							label: 'Biology',
+							description: 'Biology',
+							value: 'bio',
+						},
+						{
+							label: 'Earth Science',
+							description: 'Earth Science',
+							value: 'es',
+						},
+						{
+							label: 'Chemistry',
+							description: 'Chemistry',
+							value: 'chem',
+						},
+						{
+							label: 'Physics',
+							description: 'Physics',
+							value: 'phy',
+						},
+						{
+							label: 'Mathematics',
+							description: 'Mathematics',
+							value: 'math',
+						},
+						{
+							label: 'Energy',
+							description: 'Energy',
+							value: 'energy',
+						},
+					]),
+			);
+
+		await interaction.followUp({ embeds: [settingsEmbed], components: [menu] });
+
+		client.on('interactionCreate', async interaction => {
+			if (!interaction.isSelectMenu()) return;
+			const values = interaction.values[0];
+			switch (values) {
+			case 'astro':
+				// astronomy stuff
+				await interaction.update({ content: 'Subject set to: Astronomy', components: [] });
+				break;
+			case 'bio':
+				// biology stuff
+				await interaction.update({ content: 'Subject set to: Biology', components: [] });
+				break;
+			case 'es':
+				// earth science stuff
+				await interaction.update({ content: 'Subject set to: Earth Science', components: [] });
+				break;
+			case 'chem':
+				// chemistry stuff
+				await interaction.update({ content: 'Subject set to: Chemistry', components: [] });
+				break;
+			case 'phy':
+				// physics stuff
+				await interaction.update({ content: 'Subject set to: Physics', components: [] });
+				break;
+			case 'math':
+				// math stuff
+				await interaction.update({ content: 'Subject set to: Mathematics', components: [] });
+				break;
+			case 'energy':
+				// the united states department of energy is a cabinet-level agency responsible for the creation and management of...
+				await interaction.update({ content: 'Subject set to: Energy', components: [] });
+				break;
+			}
+		});
+		break;
+	}
+	}
 }
