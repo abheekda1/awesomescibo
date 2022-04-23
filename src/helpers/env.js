e1bc71ff803eafa782fd942bab6a1b6dccfd7b23
Eric Yang <tetrgda@gmail.com>
2022-04-23 17:26:57 -0400

    deleted js files in helper directory

--- src/helpers/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoUri = exports.token = exports.testingGuild = exports.clientId = void 0;
-require("dotenv/config");
-exports.clientId = process.env.CLIENT_ID || '';
-exports.testingGuild = process.env.TESTING_GUILD || '';
-exports.token = process.env.TOKEN || '';
-exports.mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017';
