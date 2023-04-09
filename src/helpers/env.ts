53ae60829564f431ca164c2c9e6f7a399c845008
Abheek Dhawan <abheekd@protonmail.com>
2023-04-09 13:54:13 -0500

    fix: allow specifying different mongodb server

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -3,5 +3,5 @@ import 'dotenv/config';
 export const clientId = process.env.CLIENT_ID || '';
 export const testingGuild = process.env.TESTING_GUILD || '';
 export const token = process.env.TOKEN || '';
-export const mongoUri = (process.env.MONGO_URI =
-	'mongodb://mongo:27017/AWESOME');
+export const mongoUri =
+	process.env.MONGO_URI || 'mongodb://mongo:27017/AWESOME';
